121039ac8f32ce9c1411e46466f84d3432a8f464
Narmatha M <narma21012006@gmail.com>
2026-09-12 11:56:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap Two Numbers/swap-two-numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    // code here
+    swap(a,b);
+
+    cout << a << " " << b;
+    return 0;
+}
